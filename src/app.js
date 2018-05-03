8cd83aa8923226e2ecbcb34366a0cc001f71f012
KingOfCramers <32515581+KingOfCramers@users.noreply.github.com>
2018-05-03 16:04:37 -0400

    fixed

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import FinalRender from "./components/FinalRender";
 import "./styles/styles.scss";
-import "normalize.css/normalize.css"
+import "normalize.css/normalize.css";
 
 // FINAL RENDER
 ReactDOM.render(<FinalRender/>, document.getElementById("app"));
--- src/components/FinalRender.js
+++ src/components/FinalRender.js
@@ -4,7 +4,7 @@ export default class FinalRender extends React.Component {
     render(){
         return (
             <div>
-
+                <a href="http://localhost:8080/">Go somewhere else</a>
             </div>
         );
     }
